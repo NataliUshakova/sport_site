e3439b7fbb444d4f9f25b8b21f9d6a55dcc1f653
NataliUshakova <nataliia.ushakova0@gmail.com>
2022-02-09 13:08:37 +0200

    added cards

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,17 +1,22 @@
-import axios from 'axios';
-import cards from '../../cardGallery.hbs';
+// import axios from 'axios';
+// import cards from '../../cardGallery.hbs';
 
 
-const ul = document.querySelector('.sport-list');
-const renderSports = function () {
-      axios
-    .get('http://localhost:3000/icons')
-          .then(({ data }) => {
-              data.map(el => {
-                  ul.insertAdjacentHTML('afterbegin', cards(el));
-              })
-          })
+// const ul = document.querySelector('.sport-list');
+// const renderSports = function () {
+//     let headers = new Headers();
     
-}
+//   headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   headers.append('Access-Control-Allow-Credentials', 'true');
+//       axios
+//     .get('https://sport-json-server.herokuapp.com')
+//         //   .then(({ data }) => {
+//         //       data.map(el => {
+//         //           ul.insertAdjacentHTML('afterbegin', cards(el));
+//         //       })
+//         //   })
+//     .then(data=>console.log(data))
+    
+// }
 
-renderSports();
+// renderSports();
