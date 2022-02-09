b1c6b27d86aa1f41d34be490194bc53eae3cc33d
NataliUshakova <nataliia.ushakova0@gmail.com>
2022-02-09 22:32:46 +0200

    updated

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,18 +1,3 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector("[data-modal-open]"),
-//     closeModalBtn: document.querySelector("[data-modal-close]"),
-//     modal: document.querySelector("[data-modal]"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
-
-//   function toggleModal() {
-//     refs.modal.classList.toggle("is-hidden");
-//   }
-//   console.log(refs.modal);
-// })();
 (() => {
   const menuBtnRef = document.querySelector("[data-menu-button]");
   const mobileMenuRef = document.querySelector("[data-menu]");
